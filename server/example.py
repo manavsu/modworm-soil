e7303bfb8da6587a242c5e4d5ad961c01d1196ff
Manav Singh <manavjotsingh17@gmail.com>
2024-05-25 10:24:29 -0700

    updated server

--- server/example.py
+++ server/example.py
@@ -2,7 +2,7 @@ from echo_server import EchoServer
 import asyncio
 
 async def main():
-    server = EchoServer("127.0.0.1", 10002, 1)
+    server = EchoServer("127.0.0.1", 502, 1)
     await server.start()
 
 asyncio.run(main())
