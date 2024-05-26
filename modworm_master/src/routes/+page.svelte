5d15c49404f74634dec59f46bc4cb4ddd0a1b535
Manav Singh <manavjotsingh17@gmail.com>
2024-05-25 19:51:23 -0700

    Added sim skeleton

---
+++ modworm_master/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import "../app.css";
+    import Register from "./register.svelte"
+</script>
+<div class="flex justify-center items-center min-h-screen text-white bg-gray-950">
+    <Register value={"0x0000"}/>
+</div>
+
---
+++ modworm_master/src/routes/register.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import "../app.css";
+    export let value:string;
+</script>
+
+<div>{value}</div>
