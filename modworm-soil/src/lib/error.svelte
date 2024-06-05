57984fe2998dd5fe4aa8404d820fad277296ca53
Manav Singh <manavjotsingh17@gmail.com>
2024-06-05 09:59:05 -0700

    moved folder

--- modworm-soil/src/lib/error.svelte
+++ modworm-soil/src/lib/error.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
     import RandomSnake from "$lib/random_snake.svelte";
     export let message: string;
-    export let path: string | null = null;
+    import { submitted } from "../routes/store";
+
+    function handleSubmit() {
+        submitted.set(false);
+    }
     </script>
 
 <div class="flex flex-col h-dvh justify-center items-center">
     <h1 class="z-10 text-3xl">{message}</h1>
-    {#if path != null}
-        <a class="z-10 text-5xl p-6" href={path}>&larr;</a>
-    {/if}
+    <button class="z-10 text-5xl p-6" on:click={handleSubmit} >&larr;</button>
 </div>
 
 <RandomSnake />
