89b83a4970cedc831522417d02bbeacfa46480c7
Manav Singh <manavjotsingh17@gmail.com>
2024-06-21 16:24:38 -0700

    worked with

--- src/routes/tables/+page.svelte
+++ src/routes/tables/+page.svelte
@@ -1,15 +1,41 @@
 <script lang="ts">
     import Title from '$lib/title.svelte';
-    import { ConnectedSocket } from '$lib/store';
+    import { ConnectedSocket, Working } from '$lib/store';
     import { fade } from 'svelte/transition';
-
+    import { BASE_URL } from '$lib/env'; 
+    import LoadingSnake from '$lib/loading_snake.svelte';
+    import Table from './table.svelte';
+    
+    let coil_table: []|null;
+    let discrete_input_table: []|null;
+    let holding_registers_table: []|null;
+    let input_registers_table: []|null;
+    
     async function GetDeviceInfo() {
         
     }
 
-    async function MapModbusClient() {
-        
+    async function ScanTable(ip:string, port:string, table:number) {
+        if ($ConnectedSocket == null) return;
+        $Working = true;
+        const response = await fetch(`${BASE_URL}/mmap/table/${ip}/${port}/${table}/`);
+        if (!response.ok) {
+            $Working = false;
+            return await response.json();
+        }
+        const table_list = await response.json();
+        $Working = false;
+        return table_list;
     }
+
+    async function ScanAllTables(address:string, port:string) {
+        holding_registers_table = (await ScanTable(address, port, 3)).holding_registers;
+        coil_table = (await ScanTable(address, port, 1)).coils;
+        discrete_input_table = (await ScanTable(address, port, 2)).discrete_inputs;
+        input_registers_table = (await ScanTable(address, port, 4)).input_registers;
+    }
+
+    $: if ($ConnectedSocket != null) (async () => await ScanAllTables($ConnectedSocket.address, $ConnectedSocket.port))();
 </script>
 
 <div in:fade={{delay: 200, duration:200}} class="flex flex-col h-full">
@@ -22,47 +48,11 @@
             <a href="/network" class="border-2 px-10 py-2 mt-8 clickable border-white text-center">Networks</a>
         </div> 
     {:else}
-        <div class="flex flex-row mx-auto p-2">
-            <div class="flex-grow flex flex-col">
-                {#each Array(64) as _}
-                    <div class="flex flex-row">
-                        {#each Array(32) as _}
-                            <div class="h-2 w-2 bg-white m-0.5 hover:scale-150">
-                            </div>
-                        {/each}
-                    </div>
-                {/each}
-            </div>
-            <div class="flex-grow flex flex-col">
-                {#each Array(64) as _}
-                    <div class="flex flex-row">
-                        {#each Array(32) as _}
-                            <div class="h-2 w-2 bg-white m-0.5">
-                            </div>
-                        {/each}
-                    </div>
-                {/each}
-            </div>
-            <div class="flex-grow flex flex-col">
-                {#each Array(64) as _}
-                    <div class="flex flex-row">
-                        {#each Array(32) as _}
-                            <div class="h-2 w-2 bg-white m-0.5">
-                            </div>
-                        {/each}
-                    </div>
-                {/each}
-            </div>
-            <div class="flex-grow flex flex-col">
-                {#each Array(64) as _}
-                    <div class="flex flex-row">
-                        {#each Array(32) as _}
-                            <div class="h-2 w-2 bg-white m-0.5">
-                            </div>
-                        {/each}
-                    </div>
-                {/each}
-            </div>
+        <div class="flex flex-row flex-wrap mx-auto p-2 w-full justify-around h-full">
+            <div class="w-1/4 mx-auto border"><Table table={coil_table}/></div>
+            <div class="w-1/4 mx-auto border"><Table table={discrete_input_table}/></div>
+            <div class="w-1/4 mx-auto border"><Table table={holding_registers_table}/></div>
+            <div class="w-1/4 mx-auto border"><Table table={input_registers_table}/></div>
         </div>
     {/if}
 </div>
--- src/routes/tables/table.svelte
+++ src/routes/tables/table.svelte
@@ -1,13 +1,47 @@
-<script>
+<script lang="ts">
     import Title from "$lib/title.svelte";
     import { BASE_URL } from "$lib/env";
     import { fade } from 'svelte/transition';
     import { Network, Socket } from "$lib/network";
     import { onMount } from "svelte";
     import { Working, ConnectedSocket} from "$lib/store";
-    async function ScanTable() {
-        const response = await fetch(`${BASE_URL}/mmap/`);
-        if (!response.ok) {
-        return await response.json();
+	import LoadingSnake from "$lib/loading_snake.svelte";
+	import { Container } from "postcss";
+
+    export let table: []|null;
+    function contains(i:number, j:number) {
+        const start_index = ((i * 32) * 32) + (j * 32)
+        const end_index = start_index + 31;
+        if (table == null) return false;
+        for (let cnt = 0; cnt < table?.length; cnt++) {
+            console.log(table[cnt][0], table[cnt][1], start_index, end_index);
+            if (start_index <= table[cnt][0] && table[cnt][0] <= end_index) {
+                return true;
+            }
+            if (start_index <= table[cnt][1] && table[cnt][1] <= end_index) {
+                return true;
+            }
+            if (table[cnt][0] <= start_index && start_index <= table[cnt][1]) {
+                return true;
+            }
+            if (table[cnt][0] <= end_index && end_index <= table[cnt][1]) {
+                return true;
+            }
+        }
+        return false;
     }
 </script>
+
+{#if table == null}
+    <LoadingSnake />
+{:else}
+    <div in:fade class="flex-grow flex flex-col">
+        {#each Array(64) as _, i}
+            <div class="flex flex-row">
+                {#each Array(32) as _, j}
+                    <div class="h-2 w-2 {contains(i, j) ? "bg-white" : "bg-gray-500"} bg-white m-0.5 hover:scale-150"></div>
+                {/each}
+            </div>
+        {/each}
+    </div>
+{/if}
